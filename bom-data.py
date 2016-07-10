4b684724052bb32c7a049606e7e0aeca689dd3da
Cherie <c.k.fishlock@gmail.com>
2016-07-10 18:18:34 +1000

    minor update

--- bom-data.py
+++ bom-data.py
@@ -1,25 +1,57 @@
 import numpy as np
-from datetime import datetime 
+import json
+import os
+import urllib
 
-ffile = "IDN60903.94926.axf"
+path = "/Users/cherief/kateandcat/assets/bom"
+#path = ""
 
-
-(sort_order,wmo,name,history_product,local_date_time,local_date_time_full,aifstime_utc,
-            air_temp,apparent_t,cloud,cloud_base_m,cloud_oktas,cloud_type,cloud_type_id,
-            delta_t,dewpt,gust_kmh,gust_kt,lat,lon,press,press_msl,press_qnh,press_tend,
-            rain_trace,rel_hum,sea_state,swell_dir_worded,swell_height,swell_period,vis_km,
-            weather,wind_dir,wind_spd_kmh,wind_spd_kt) = np.genfromtxt(ffile,
-            unpack=True,delimiter=',',skip_header=20, skip_footer=1,dtype='str')
-
-air_temp = np.array(air_temp,dtype='float')
-apparent_t = np.array(apparent_t,dtype='float')
-
-dates = []
-for d in local_date_time_full:
-    date = datetime.strptime(d, '"%Y%m%d%H%M%S"')
-    dates = np.append(dates,[date],axis=0)
+url_cbr = "http://www.bom.gov.au/fwo/IDN60903/IDN60903.94926.json"
 
 
-data = np.column_stack((dates,air_temp,apparent_t))
+compass = {"N":0,"NNE":1,"NE":2,"ENE":3,"E":4,"ESE":5,"SE":6,"SSE":7,"S":8,"SSW":9,"SW":10,"WSW":11,"W":12,"WNW":13,"NW":14,"NNW":15,"CALM":0}
 
-np.savetxt('bom-data.csv',data,fmt="%s",delimiter=",",header="date,temp,apparent")
+urllib.urlretrieve(url_cbr, os.path.join(path,"IDN60903.94926.json"))
+
+def read_json():
+
+    ffile = os.path.join(path,"IDN60903.94926.json")
+    jsondata = open(ffile).read()
+    data = json.loads(jsondata)["observations"]["data"]
+    date,air_temp,apparent_t,wind_dir,wind_kmh,wind_color= ["date"],["temp"],["apparent"],["wind_dir"],["wind_kmh"],["wind_color"]
+    for i in xrange(0,len(data)):
+        d,t,a,w,k = data[i]["local_date_time_full"],data[i]["air_temp"],data[i]["apparent_t"],data[i]["wind_dir"],data[i]["wind_spd_kmh"]
+        date.append(d)
+        air_temp.append(t)
+        apparent_t.append(a)
+
+
+
+        w_convert = compass.get(w)*(2.*np.pi/16.)
+        print w,w_convert
+
+
+
+        wind_dir.append(w_convert)
+        wind_kmh.append(k)
+
+        if k <= 20:
+            c = 0.
+        elif 20 < k <= 25:
+            c = 1.
+        elif 25 < k <= 30:
+            c = 2.
+        elif 30 < k <= 35:
+            c = 3.
+        else:
+            c = 4.
+
+
+        wind_color.append(c)
+
+    data = np.column_stack((date,air_temp,apparent_t,wind_dir,wind_kmh,wind_color))
+    np.savetxt(os.path.join(path,'bom-data.csv'),data,fmt="%s",delimiter=",")
+
+read_json()    
+
+  
